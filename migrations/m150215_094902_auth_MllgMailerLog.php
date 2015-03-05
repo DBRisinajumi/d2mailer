a6c37baea097850486699a5a97fb4c816543604b
Uldis Nelsons <uldisnelsons@gmail.com>
2015-03-05 09:51:08 +0200

    fixed table names

--- migrations/m150215_094902_auth_MllgMailerLog.php
+++ migrations/m150215_094902_auth_MllgMailerLog.php
@@ -6,12 +6,12 @@ class m150215_094902_auth_MllgMailerLog extends CDbMigration
     public function up()
     {
         $this->execute("
-            INSERT INTO `authitem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.*','0','D2mailer.MllgMailerLog',NULL,'N;');
-            INSERT INTO `authitem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.Create','0','D2mailer.MllgMailerLog module create',NULL,'N;');
-            INSERT INTO `authitem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.View','0','D2mailer.MllgMailerLog module view',NULL,'N;');
-            INSERT INTO `authitem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.Update','0','D2mailer.MllgMailerLog module update',NULL,'N;');
-            INSERT INTO `authitem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.Delete','0','D2mailer.MllgMailerLog module delete',NULL,'N;');
-            INSERT INTO `authitem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.Menu','0','D2mailer.MllgMailerLog show menu',NULL,'N;');
+            INSERT INTO `AuthItem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.*','0','D2mailer.MllgMailerLog',NULL,'N;');
+            INSERT INTO `AuthItem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.Create','0','D2mailer.MllgMailerLog module create',NULL,'N;');
+            INSERT INTO `AuthItem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.View','0','D2mailer.MllgMailerLog module view',NULL,'N;');
+            INSERT INTO `AuthItem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.Update','0','D2mailer.MllgMailerLog module update',NULL,'N;');
+            INSERT INTO `AuthItem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.Delete','0','D2mailer.MllgMailerLog module delete',NULL,'N;');
+            INSERT INTO `AuthItem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES('D2mailer.MllgMailerLog.Menu','0','D2mailer.MllgMailerLog show menu',NULL,'N;');
                 
 
         ");
@@ -20,12 +20,12 @@ class m150215_094902_auth_MllgMailerLog extends CDbMigration
     public function down()
     {
         $this->execute("
-            DELETE FROM `authitem` WHERE `name`= 'D2mailer.MllgMailerLog.*';
-            DELETE FROM `authitem` WHERE `name`= 'D2mailer.MllgMailerLog.Create';
-            DELETE FROM `authitem` WHERE `name`= 'D2mailer.MllgMailerLog.View';
-            DELETE FROM `authitem` WHERE `name`= 'D2mailer.MllgMailerLog.Update';
-            DELETE FROM `authitem` WHERE `name`= 'D2mailer.MllgMailerLog.Delete';
-            DELETE FROM `authitem` WHERE `name`= 'D2mailer.MllgMailerLog.Menu';
+            DELETE FROM `AuthItem` WHERE `name`= 'D2mailer.MllgMailerLog.*';
+            DELETE FROM `AuthItem` WHERE `name`= 'D2mailer.MllgMailerLog.Create';
+            DELETE FROM `AuthItem` WHERE `name`= 'D2mailer.MllgMailerLog.View';
+            DELETE FROM `AuthItem` WHERE `name`= 'D2mailer.MllgMailerLog.Update';
+            DELETE FROM `AuthItem` WHERE `name`= 'D2mailer.MllgMailerLog.Delete';
+            DELETE FROM `AuthItem` WHERE `name`= 'D2mailer.MllgMailerLog.Menu';
 
         ");
     }
